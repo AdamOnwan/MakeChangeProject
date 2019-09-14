44a73ef26e380c72bc7d8e5f5763fb25d62a1346
Adam <adamonwan@gmail.com>
2019-09-14 11:22:40 -0600

    Completed Project

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -4,25 +4,28 @@ public class MakeChangeApp {
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		
+
 		System.out.println("Enter price of item : ");
 		double price = kb.nextDouble();
 		System.out.println("Enter customer payment : ");
 		double payment = kb.nextDouble();
 
 		int intPrice = (int) (price * 100 + .5);
-		int intPayment = (int) (payment * 100 +.5);
-		
+		int intPayment = (int) (payment * 100 + .5);
+
 		int twentyDollars = 0, tenDollars = 0, fiveDollars = 0, oneDollar = 0, quarter = 0, dime = 0, nickel = 0,
 				penny = 0;
 
-
 		int customerChange = intPayment - intPrice;
-		
-		while (customerChange > 0) 
+
+		if (customerChange < 0) {
+			System.out.println("ERROR NOT ENOUGH MONEY");
+		} else if (customerChange == 0) {
+			System.out.println("Exact change thank you");
+		} else {
 			System.out.println("hand the customer");
-		
-		{
+		}
+		while (customerChange > 0) {
 			if (customerChange >= 2000) {
 				customerChange = customerChange - 2000;
 				twentyDollars++;
@@ -49,19 +52,19 @@ public class MakeChangeApp {
 			} else if (customerChange >= 25) {
 				customerChange = customerChange - 25;
 				quarter++;
-				if (customerChange <=24 && quarter >= 1) {
+				if (customerChange <= 24 && quarter >= 1) {
 					System.out.println(quarter + " Quarters");
 				}
 			} else if (customerChange >= 10) {
 				customerChange = customerChange - 10;
 				dime++;
-				if (customerChange <=9 && dime >= 1) {
+				if (customerChange <= 9 && dime >= 1) {
 					System.out.println(dime + " Dimes");
 				}
 			} else if (customerChange >= 5) {
 				customerChange = customerChange - 5;
 				nickel++;
-				if (customerChange <=4 && nickel >= 1) {
+				if (customerChange <= 4 && nickel >= 1) {
 					System.out.println(nickel + " Nickels");
 				}
 			} else if (customerChange > 0) {
@@ -72,65 +75,8 @@ public class MakeChangeApp {
 				}
 			}
 		}
-		
-		if (customerChange < 0) {
-			System.out.println("ERROR NOT ENOUGH MONEY");
-		}
-		
+
 		kb.close();
 
 	}
 }
-
-
-
-
-
-//	public static double itemPrice() {
-//		Scanner kb = new Scanner(System.in);
-//		System.out.println("Please enter price of item: ");
-//		double price = kb.nextDouble();
-//
-//		return price;
-//	}
-//
-//	public static double itemPayment() {
-//
-//		Scanner kbb = new Scanner(System.in);
-//		System.out.println("Please enter customer payment: ");
-//		double payment = kbb.nextDouble();
-//
-//		return payment;
-//
-//	}
-
-//Make Change (Cash Register)
-//Denominations that are not used should not be displayed.
-//Hint: Mod operator
-//
-//User Story #1
-//The user is prompted asking for the price of the item.
-//
-//User Story #2
-//The user is then prompted asking how much money was tendered by 
-//the customer.
-//
-//User Story #3
-//Display an appropriate message if the customer provided too little 
-//money or the exact amount.
-//
-//User Story #4
-//If the amount tendered is more than the cost of the item, display 
-//the number of bills and coins that should be given to the customer.
-//
-//You will be given either a pass or fail based on whether your code
-//works given all of the following test conditions:
-//
-//Amount: .6719dered: 20.00, Result: 1 ten dollar bill, 1 five 
-//dollar bill, 1 one dollar bill, 4 pennies.
-//Amount: any amount less than 20.00, Tendered: anything greater than 
-//amount: correct denominations for correct change.
-//If the project does work with all of the above test conditions, you 
-//will be given a 1 for this week's project.
-//To turn in a project, you must push it to GitHub. You must include 
-//a README.md that describes how to run your program.
